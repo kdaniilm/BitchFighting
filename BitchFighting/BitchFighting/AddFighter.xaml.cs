eeef585fb98e79f8dbbeb94c074965e4f7262a2d
kdaniilm <kdaniilm@gmail.com>
2020-09-07 21:30:27 +0300

    Add AddWindow IU and minimal logic

--- BitchFighting/BitchFighting/AddFighter.xaml.cs
+++ BitchFighting/BitchFighting/AddFighter.xaml.cs
@@ -23,5 +23,26 @@ namespace BitchFighting
         {
             InitializeComponent();
         }
+
+        private void PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!Char.IsDigit(e.Key.ToString().ToCharArray().ToList().Last())) 
+            { 
+                e.Handled = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameTb.Text.Length > 0 && DescriptionTb.Text.Length > 0 && HPTb.Text.Length > 0 && DefenceTb.Text.Length > 0 && AttackTb.Text.Length > 0 && ImageUrlTb.Text.Length > 0)
+            {
+                this.Hide();
+                new MainWindow().ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Complite all datas!");
+            }
+        }
     }
 }

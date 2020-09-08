97b52cea7eb687a3c9a754bb715eb46d6808b90d
QQ <44963782+qqreaper@users.noreply.github.com>
2020-09-08 12:41:58 +0300

    AddFighter

---
+++ BitchFighting/BitchFighting/viewmodel/AddFighterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using BitchFighting.data;
+using BitchFighting.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitchFighting.viewmodel
+{
+    class AddFighterViewModel
+    {
+        private List<Hero> _heroes;
+
+        public AddFighterViewModel()
+        {
+            GetHeroes();
+        }
+
+        public List<Hero> GetHeroes()
+        {
+            _heroes = DatabaseControl.getInstance().GetAll();
+            return _heroes;
+        }
+
+        public void AddHero(Hero hero)
+        {
+            _heroes.Add(hero);
+
+        }
+    }
+}
